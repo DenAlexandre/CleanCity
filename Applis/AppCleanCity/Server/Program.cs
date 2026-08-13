2d46773da24c94882b0941d6edb76b2f13427721
ALEXANDRE <d.alexandre@semeru.fayat.com>
2026-08-13 11:31:08 +0200

    commit

--- Applis/AppCleanCity/Server/Program.cs
+++ Applis/AppCleanCity/Server/Program.cs
@@ -1,7 +1,7 @@
 using CortexiaAuth.Api.Data;
 using CortexiaAuth.Api.Models;
 using CortexiaAuth.Api.Services;
-using Microsoft.AspNetCore.DataProtection.EntityFrameworkCore;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
